94efcf091e41b71dbc0f0fd79d1cdffed508bb43
kyle <kyle@kylelee.dev>
2023-05-26 12:37:25 -0500

    changed meta data for website

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import { Layout } from '@/components/dom/Layout'
 import '@/global.css'
 
 export const metadata = {
-  title: 'Next.js + Three.js',
-  description: 'A minimal starter for Nextjs + React-three-fiber and Threejs.',
+  title: 'Kyle Lee | Homepage',
+  description: `Kyle Lee's personal website.`,
 }
 
 export default function RootLayout({ children }) {
