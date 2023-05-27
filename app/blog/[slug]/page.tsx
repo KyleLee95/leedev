b2d128b145633db5befb1645bf86a95e6f883960
kyle <kyle@kylelee.dev>
2023-05-27 14:38:34 -0500

    blog routing working

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { BLOCKS, MARKS } from '@contentful/rich-text-types'
+
+const Bold = ({ children }) => <span className='font-bold'>{children}</span>
+
+const Text = ({ children }) => <p className='content-center'>{children}</p>
+
+const options = {
+  renderMark: {
+    [MARKS.BOLD]: (text) => <Bold>{text}</Bold>,
+  },
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+  },
+}
+
+export function Page(props) {
+  const { id, title, body } = props.post.fields
+  const dtrc = documentToReactComponents(body, options)
+  return (
+    <main className='justify-self-center'>
+      {title}
+      {dtrc}
+    </main>
+  )
+}
