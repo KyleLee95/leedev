d0bf0a26a3686c6ff68acb456a6d3f7cff3f5cf9
kyle <kyle@kylelee.dev>
2023-05-28 15:30:24 -0500

    added styled components for rendering

--- app/blog/Post.tsx
+++ app/blog/Post.tsx
@@ -1,26 +1,39 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
-import { BLOCKS, MARKS } from '@contentful/rich-text-types'
+import { BLOCKS, MARKS, INLINES } from '@contentful/rich-text-types'
 
 const Bold = ({ children }) => <span className='font-bold'>{children}</span>
 
 const Text = ({ children }) => <p className='content-center'>{children}</p>
 
+const HyperLink = ({ children, node }) => (
+  <a className='text-blue-300 hover:underline' href={node.data.uri}>
+    {children}
+  </a>
+)
+
+const CodeBlock = ({ children }) => {
+  return <code className='my-4 bg-slate-600 p-1'>{children}</code>
+}
+const Title = ({ children }) => {
+  return <h1 className='text-3xl font-bold'>{children}</h1>
+}
+
 const options = {
   renderMark: {
     [MARKS.BOLD]: (text) => <Bold>{text}</Bold>,
+    [MARKS.CODE]: (text) => <CodeBlock>{text}</CodeBlock>,
   },
   renderNode: {
+    [BLOCKS.HEADING_1]: (node, children) => <Title>{children}</Title>,
     [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+    [INLINES.HYPERLINK]: (node, children) => {
+      return <HyperLink node={node}>{children}</HyperLink>
+    },
   },
 }
 
 export const Post = (props) => {
   const { id, title, body } = props.post.fields
   const dtrc = documentToReactComponents(body, options)
-  return (
-    <main className='justify-self-center'>
-      {title}
-      {dtrc}
-    </main>
-  )
+  return <main className='justify-self-center'>{dtrc}</main>
 }
